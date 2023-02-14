9c097bb7e60cdf6efddf151c779283b2b5fc2534
ext.jiayaning1 <ext.jiayaning1@jd.com>
2023-02-14 09:25:07 +0800

    feat: 小组件

--- DailyBookKeepingWidget/DailyBookKeepingWidget.swift
+++ DailyBookKeepingWidget/DailyBookKeepingWidget.swift
@@ -41,6 +41,11 @@ struct SimpleEntry: TimelineEntry {
 struct DailyBookKeepingWidgetEntryView : View {
     var entry: Provider.Entry
     
+    let edges: EdgeInsets = EdgeInsets.init(top: 15, leading: 0, bottom: 15, trailing: 0)
+    let itemColor: Color = Color.init(red: 51/255.0, green: 53/255.0, blue: 86/255.0)
+    let radius: CGFloat = 10.0
+    let itemWidth: CGFloat = 70
+    
     @Environment(\.widgetFamily) var family: WidgetFamily;
     
     var body: some View {
@@ -48,37 +53,149 @@ struct DailyBookKeepingWidgetEntryView : View {
         case .systemMedium:
             VStack{
                 HStack(spacing: 0) {
-                    Text("本年")
-                        .font(Font.system(size: 14))
-                        .foregroundColor(Color.white)
-                        .fontWeight(Font.Weight.semibold)
                     Spacer()
-                    Text("支")
-                        .font(Font.system(size: 10, weight: Font.Weight.semibold))
+                        .frame(width: 2)
+                    Text("今年")
+                        .font(.chineseFont(size: 16))
                         .foregroundColor(Color.white)
-                        .frame(height: 10, alignment: .top)
-                    HStack(alignment: .lastTextBaseline, spacing: 0){
+                        .italic()
+                    Spacer()
+                    HStack(alignment: .lastTextBaseline, spacing: 2){
+                        Text("支")
+                            .font(.chineseFont(size: 12))
+                            .offset(x: 0, y: -0.4)
                         Text("￥")
-                            .font(Font.system(size: 11, weight: Font.Weight.semibold))
-                            .foregroundColor(Color.white)
-                        Text("2381")
-                            .font(Font.system(size: 16, weight: Font.Weight.semibold))
-                            .foregroundColor(Color.white)
-                        Text(".36")
-                            .font(Font.system(size: 11, weight: Font.Weight.semibold))
-                            .foregroundColor(Color.white)
+                            .font(.chineseFont(size: 10))
+                            .offset(x: 0, y: -0.4)
+                        HStack(alignment: .lastTextBaseline, spacing: 0) {
+                            Text("0")
+                                .font(.jdBoldFont(size: 20))
+                            Text(".")
+                                .font(.jdBoldFont(size: 10))
+                            Text("00")
+                                .font(.jdBoldFont(size: 10))
+                        }
                     }
                 }
+                .foregroundColor(Color.white)
                 Spacer()
-                HStack{
-                    Text("支￥2381.11")
+                    .frame(height: 10)
+                VStack{
+                    Spacer()
+                        .frame(height: 0)
+                    HStack(spacing: 0) {
+                        VStack(spacing: 12) {
+                            Text("餐饮")
+                                .font(.chineseFont(size: 13))
+                                .foregroundColor(Color.white)
+                            Text("🍜")
+                                .font(.system(size: 18))
+                            HStack(alignment: .lastTextBaseline, spacing: 0){
+                                Text("￥")
+                                    .font(.jdBoldFont(size: 8))
+                                    .foregroundColor(Color.white)
+                                + Text("99999")
+                                    .font(.jdBoldFont(size: 12))
+                                    .foregroundColor(Color.white)
+                                + Text(".45")
+                                    .font(.jdBoldFont(size: 8))
+                                    .foregroundColor(Color.white)
+                            }
+                        }
+                        .frame(width: itemWidth)
+                        .padding(edges)
+                        .background(itemColor)
+                        .cornerRadius(radius)
+                        Spacer()
+                            .frame(minWidth: 0)
+                        VStack(spacing: 12) {
+                            Text("交通")
+                                .font(.chineseFont(size: 13))
+                                .foregroundColor(Color.white)
+                            Text("🚕")
+                                .font(.system(size: 18))
+                            HStack(alignment: .lastTextBaseline, spacing: 0){
+                                Text("￥")
+                                    .font(.jdBoldFont(size: 8))
+                                    .foregroundColor(Color.white)
+                                + Text("99999")
+                                    .font(.jdBoldFont(size: 12))
+                                    .foregroundColor(Color.white)
+                                + Text(".45")
+                                    .font(.jdBoldFont(size: 8))
+                                    .foregroundColor(Color.white)
+                            }
+                        }
+                        .frame(width: itemWidth)
+                        .padding(edges)
+                        .background(itemColor)
+                        .cornerRadius(radius)
+                        Spacer()
+                            .frame(minWidth: 0)
+                        VStack(spacing: 12) {
+                            Text("购物")
+                                .font(.chineseFont(size: 13))
+                                .foregroundColor(Color.white)
+                            Text("🛍")
+                                .font(.system(size: 18))
+                            ItemView()
+                        }
+                        .frame(width: itemWidth)
+                        .padding(edges)
+                        .background(itemColor)
+                        .cornerRadius(radius)
+                        Spacer()
+                            .frame(minWidth: 0)
+                        VStack(spacing: 12) {
+                            Text("娱乐")
+                                .font(.chineseFont(size: 13))
+                                .foregroundColor(Color.white)
+                            Text("🤹‍♂️")
+                                .font(.system(size: 18))
+                            HStack(alignment: .lastTextBaseline, spacing: 0){
+                                Text("￥")
+                                    .font(.jdBoldFont(size: 8))
+                                    .foregroundColor(Color.white)
+                                + Text("999")
+                                    .font(.jdBoldFont(size: 12))
+                                    .foregroundColor(Color.white)
+                                + Text(".45")
+                                    .font(.jdBoldFont(size: 8))
+                                    .foregroundColor(Color.white)
+                            }
+                        }
+                        .frame(width: itemWidth)
+                        .padding(edges)
+                        .background(itemColor)
+                        .cornerRadius(radius)
+                        Spacer()
+                            .frame(width: 0)
+                    }
+                    Spacer()
+                        .frame(minHeight: 0)
                 }
-                Spacer()
+                .padding(0)
             }
             .padding(EdgeInsets.init(top: 10, leading: 15, bottom: 10, trailing: 15))
-            .background(Color.orange)
+            .background(LinearGradient(gradient: Gradient(colors: [Color.init(red: 91/255.0, green: 93/255.0, blue: 126/255.0), Color.init(red: 76/255.0, green: 78/255.0, blue: 112/255.0), Color.init(red: 91/255.0, green: 93/255.0, blue: 126/255.0)]), startPoint: UnitPoint(x: 0, y: 0), endPoint: UnitPoint(x: 0, y: 1)))
         default:
             Text("")
+        }
+    }
+}
+
+struct ItemView: View {
+    var body: some View {
+        HStack(alignment: .lastTextBaseline, spacing: 0){
+            Text("￥")
+                .font(.jdBoldFont(size: 8))
+                .foregroundColor(Color.white)
+            + Text("999")
+                .font(.jdBoldFont(size: 12))
+                .foregroundColor(Color.white)
+            + Text(".45")
+                .font(.jdBoldFont(size: 8))
+                .foregroundColor(Color.white)
         }
     }
 }
@@ -93,5 +210,17 @@ struct DailyBookKeepingWidget: Widget {
         .configurationDisplayName("My Widget")
         .description("This is an example widget.")
         .supportedFamilies([.systemMedium])
+    }
+}
+
+extension Font {
+    static func jdRegularFont(size: CGFloat) -> Font {
+        return Font.custom("JDZhengHT-Regular", size: size)
+    }
+    static func jdBoldFont(size: CGFloat) -> Font {
+        return Font.custom("JDLANGZHENGTI-SB--GBK1-0", size: size)
+    }
+    static func chineseFont(size: CGFloat) -> Font {
+        return Font.custom("AppleTsukuBRdGothic-Bold", size: size)
     }
 }
--- Podfile
+++ Podfile
@@ -19,3 +19,32 @@ target 'DailyBookKeepingWidgetExtension' do
   # Pods for DailyBookKeepingWidgetExtension
 
 end
+
+post_install do |installer|
+  
+  pod_Target_dir = Dir.pwd + "/Pods/Target\ Support\ Files"
+  
+  installer.aggregate_targets.each do |target|
+    target.user_build_configurations.each do |key, name|
+      #pod生成的xcconfig文件路径
+      downcase_key = "#{key}".downcase #转换成小写key
+      pod_xcconfig_path = "#{pod_Target_dir}/#{target}/#{target}.#{downcase_key}.xcconfig"
+      original_target_name_length = "#{target}".length
+      need_target_name = "#{target}"[5,original_target_name_length]
+
+      #Testing对应Adhoc类型的证书
+      if downcase_key == "testing"
+        downcase_key = "adhoc"
+      end
+      custom_config_file_path = Dir.pwd + "/#{need_target_name}/Config/#{key}.xcconfig"
+      if File::exists?(custom_config_file_path)
+        #文件存在,以追加模式打开文件,并include对应的自定义xcconfig文件
+        File.open(pod_xcconfig_path,"a") do |afile|
+          afile.puts "#include \"#{custom_config_file_path}\""
+        end
+      else
+        #文件不存在
+      end
+    end
+  end
+end
